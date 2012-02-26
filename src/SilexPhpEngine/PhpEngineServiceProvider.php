f10aaddc42cf60837e9a13047205ff1b40367411
litek <ketil@syscom.as>
2012-02-26 17:22:54 +0100

    Converted tabs to four spaces in accordance with Symfony coding standards.

--- src/SilexPhpEngine/PhpEngineServiceProvider.php
+++ src/SilexPhpEngine/PhpEngineServiceProvider.php
@@ -11,38 +11,38 @@ use Symfony\Component\Templating\TemplateNameParser;
 
 class PhpEngineServiceProvider implements ServiceProviderInterface
 {
-	public function register(Application $app)
-	{		
-		$app['view'] = $app->share(function($app) {
-			$engine  = $app['view.engine'];
+    public function register(Application $app)
+    {       
+        $app['view'] = $app->share(function($app) {
+            $engine  = $app['view.engine'];
 
-			$helpers = isset($app['view.helpers']) ? $app['view.helpers'] : $app['view.default_helpers'];
-			$engine->setHelpers($helpers);
+            $helpers = isset($app['view.helpers']) ? $app['view.helpers'] : $app['view.default_helpers'];
+            $engine->setHelpers($helpers);
 
-			return $engine;
-		});
+            return $engine;
+        });
 
-		$app['view.engine'] = $app->share(function($app) {
-			return new PhpEngine($app['view.parser'], $app['view.loader']);
-		});
+        $app['view.engine'] = $app->share(function($app) {
+            return new PhpEngine($app['view.parser'], $app['view.loader']);
+        });
 
-		$app['view.loader'] = $app->share(function($app) {
-			$paths = isset($app['view.paths']) ? $app['view.paths'] : array();
-			return new FileSystemLoader($paths);
-		});
+        $app['view.loader'] = $app->share(function($app) {
+            $paths = isset($app['view.paths']) ? $app['view.paths'] : array();
+            return new FileSystemLoader($paths);
+        });
 
-		$app['view.parser'] = $app->share(function() {
-			return new TemplateNameParser;
-		});
+        $app['view.parser'] = $app->share(function() {
+            return new TemplateNameParser;
+        });
 
-		$app['view.default_helpers'] = $app->share(function() {
-			return array(
-				new \Symfony\Component\Templating\Helper\SlotsHelper
-			);
-		});
+        $app['view.default_helpers'] = $app->share(function() {
+            return array(
+                new \Symfony\Component\Templating\Helper\SlotsHelper
+            );
+        });
 
-		if (isset($app['view.class_path'])) {
-			$app['autoloader']->registerNamespace('Symfony\\Component\\Templating', $app['view.class_path']);
-		}
-	}
+        if (isset($app['view.class_path'])) {
+            $app['autoloader']->registerNamespace('Symfony\\Component\\Templating', $app['view.class_path']);
+        }
+    }
 }
